ff7fa33595a61edd2550085528105e45e548d7fe
Lemon Shi <shir@vmware.com>
2024-03-27 20:55:07 +0800

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
-vim.keymap.set("n", "<leader>\\", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+vim.keymap.set(
+  "n",
+  "<leader><C-a>",
+  "oimport ipdb;ipdb.set_trace()<Esc>:w<Enter>j0w",
+  { desc = "insert Python debugger" }
+)
