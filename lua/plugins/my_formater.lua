4c0faa98404bf1f9c98d6ce40de34d1298675c60
Lemon Shi <shir@vmware.com>
2024-04-10 11:44:18 +0800

    update the formater

--- lua/plugins/my_formater.lua
+++ lua/plugins/my_formater.lua
@@ -1,9 +1,30 @@
+local util = require("conform.util")
 return {
   "stevearc/conform.nvim",
   optional = true,
   opts = {
+    formatters = {
+      my_black = {
+        meta = {
+          url = "https://github.com/psf/black",
+          description = "The uncompromising Python code formatter.",
+        },
+        command = "black",
+        args = {
+          "--line-length",
+          "120",
+          "--quiet",
+          "-",
+        },
+        cwd = util.root_file({
+          -- https://black.readthedocs.io/en/stable/usage_and_configuration/the_basics.html#configuration-via-a-file
+          "pyproject.toml",
+          "skaffold.yaml",
+        }),
+      },
+    },
     formatters_by_ft = {
-      ["python"] = { "ruff_fix", "ruff_format" },
+      ["python"] = { "my_black" },
       ["javascript"] = { "prettier" },
       ["javascriptreact"] = { "prettier" },
       ["typescript"] = { "prettier" },
